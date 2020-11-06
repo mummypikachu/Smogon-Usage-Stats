5f4c85cc273d643eab863d92068ca6cc8d408f87
Marty-D <Marty-D@users.noreply.github.com>
2020-11-06 09:57:12 -0500

    Update files

--- CatchupRandbats.sh
+++ CatchupRandbats.sh
@@ -2,18 +2,17 @@
 #This file is included solely to be used as an example. It will likely need to be heavily modified from month to month
 #(or from run to run)
 
-logFolder=/home/ps/showdown/logs
-month="2018-12"
-rm -r Raw
+logFolder=/home/ps/main/logs
+month="2020-08"
 mkdir Raw
 
-for d in {1..31}
+for d in {1..7}
 do
 	day=$(printf "%02d" $d)
 	for i in $logFolder/$month/*
 	do
 		tier=$(basename $i)
-		if [[ $tier == seasonal* ]] || [[ $tier == *random* ]] || [[ $tier == *metronome* ]] || [[ $tier == *superstaff* ]]; then
+		if [[ $tier != *random* ]]; then
 			echo Skipping $tier/$month-$day
 			continue
 		fi
---
+++ CatchupRaw.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#This file is included solely to be used as an example. It will likely need to be heavily modified from month to month
+#(or from run to run)
+
+logFolder=/home/ps/main/logs
+month="2020-09"
+mkdir Raw
+
+for d in {17..24}
+do
+	day=$(printf "%02d" $d)
+	for i in $logFolder/$month/*
+	do
+		tier=$(basename $i)
+		if [[ $tier == seasonal* ]] || [[ $tier == *random* ]] || [[ $tier == *petmod* ]] || [[ $tier == *superstaff* ]] || [[ $tier == *factory* ]] || [[ $tier == *challengecup* ]] || [[ $tier == *hackmonscup* ]] || [[ $tier == *digimon* ]]; then
+			echo Skipping $tier/$month-$day
+			continue
+		fi
+		if [ -d $logFolder/$month/$tier/$month-$day ]; then
+			echo Processing $tier/$month-$day
+			python batchLogReader.py $logFolder/$month/$tier/$month-$day/ $tier
+		fi
+	done
+done
+echo $(date)
